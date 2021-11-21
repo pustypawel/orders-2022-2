39f0c7ba1c8a5c273139234dc580614368832782
ppustelnik <pawel.pustelnik@exorigo-upos.pl>
2021-11-21 13:32:02 +0100

    impl

--- src/main/java/pl/edu/wszib/order/application/product/ProductRepository.java
+++ src/main/java/pl/edu/wszib/order/application/product/ProductRepository.java
@@ -1,5 +1,9 @@
 package pl.edu.wszib.order.application.product;
 
+import java.util.Optional;
+
 public interface ProductRepository {
     Product save(Product product);
+
+    Optional<Product> findById(final ProductId id);
 }
--- src/test/java/pl/edu/wszib/order/application/product/InMemoryProductRepository.java
+++ src/test/java/pl/edu/wszib/order/application/product/InMemoryProductRepository.java
@@ -1,4 +1,23 @@
 package pl.edu.wszib.order.application.product;
 
+import pl.edu.wszib.order.application.order.Order;
+import pl.edu.wszib.order.application.order.OrderId;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
 public class InMemoryProductRepository implements ProductRepository {
+    private final Map<ProductId, Product> products = new HashMap<>();
+
+    @Override
+    public Product save(final Product product) {
+        products.put(product.getId(), product);
+        return product;
+    }
+
+    @Override
+    public Optional<Product> findById(final ProductId id) {
+        return Optional.ofNullable(products.get(id));
+    }
 }
