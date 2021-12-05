50cff14fff2039c6dfece753cd7b97afb748f332
ppustelnik <pawel.pustelnik@exorigo-upos.pl>
2021-12-05 14:00:17 +0100

    product validation annotations

--- src/main/java/pl/edu/wszib/order/application/product/Product.java
+++ src/main/java/pl/edu/wszib/order/application/product/Product.java
@@ -1,8 +1,11 @@
 package pl.edu.wszib.order.application.product;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
+import org.hibernate.validator.constraints.Length;
 
 import java.math.BigDecimal;
 import java.util.Objects;
@@ -11,11 +14,17 @@ import java.util.Objects;
 @AllArgsConstructor
 @Getter
 public class Product {
+    @NotNull
     private final ProductId id;
+
+    @NotNull
+    @Length(min = 2, max = 50)
     private final String name;
 
     //docelowo powinniśmy skorzystać z:
     //https://github.com/JavaMoney/jsr354-api
+    @NotNull
+    @Positive
     private final BigDecimal price;
 
     public static Product create(final ProductId id,
